7b34a6b9e554c6f20a3c1ee211a7c677c75a71b8
zz384 <1539130604@qq.com>
2025-07-11 13:26:48 +0800

    Delete myboke/myboke directory

--- myboke/myboke/daima/duan.py
+++
@@ -1,4 +0,0 @@
-def duan_yan(self,resp,code,success,message):
-    self.assertEqual(code, resp.json().get("code"))
-    self.assertEqual(success, resp.json().get("success"))
-    self.assertIn(message, resp.json().get("message"))
--- myboke/myboke/daima/qianzhi.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-class Test_qianzhi(object):
-    @classmethod
-    def login(cls,json_data):
-        resp = requests.post(url="http://127.0.0.1:5000/api/login",json=json_data)
-        return resp
